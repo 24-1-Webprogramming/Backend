0cf7b689485546a05b261693d21202e4cd80a5ab
moongua404 <hogun.soongit@gmail.com>
2024-06-06 06:06:44 +0900

    fix: version

--- src/util/swagger.ts
+++ src/util/swagger.ts
@@ -10,7 +10,7 @@ export function setupSwagger(app: INestApplication): void {
   const options = new DocumentBuilder()
     .setTitle('NestJS API - hogun')
     .setDescription('webprogramming API')
-    .setVersion('1.0.1')
+    .setVersion('1.0.2')
     .build();
 
   const document = SwaggerModule.createDocument(app, options);
