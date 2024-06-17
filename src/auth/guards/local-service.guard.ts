9fabf708d243e5710a697e89f4553309e9cd94f3
Yoonseo Jun <iamyoonseo@soongsil.ac.kr>
2024-06-18 02:30:27 +0900

    Delete src directory

--- src/auth/guards/local-service.guard.ts
+++
@@ -1,5 +0,0 @@
-import { Injectable } from "@nestjs/common";
-import { AuthGuard } from "@nestjs/passport";
-
-@Injectable()
-export class LocalServiceStrategy extends AuthGuard('local-service') {}
--- src/diet/dto/find-diet.dto.ts
+++
@@ -1,6 +0,0 @@
-import { ApiProperty } from "@nestjs/swagger";
-
-export class FindDietDto {
-    @ApiProperty()
-    log_date: Date;
-}
