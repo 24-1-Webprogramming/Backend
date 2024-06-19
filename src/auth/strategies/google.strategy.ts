ddf6b1fb40bfa48efcb564485242dd7f64720431
moongua404 <hogun.soongit@gmail.com>
2024-06-19 13:57:11 +0900

    fix: google redirect

--- src/auth/strategies/google.strategy.ts
+++ src/auth/strategies/google.strategy.ts
@@ -10,6 +10,7 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
       clientSecret: "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",
       callbackURL: "http://soongitglwebp8.site/auth/google/callback",
       scope: ['email', 'profile'],
+      //authorizationURL: 'https://accounts.google.com/o/oauth2/auth?prompt=select_account',
     });
   }
 
@@ -20,7 +21,7 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
     done: VerifyCallback,
   ){
     const { id, name, emails } = profile;
-    console.log(profile);
+    //console.log(profile);
     const user = {
       provider: 'google',
       providerId: id,
